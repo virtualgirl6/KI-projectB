1a87b37765f5005a34563168cc6f7b5670fa2167
Yuri Teerlink <iehkaatee@MacBook-Air-van-Yuri.local>
2019-12-05 17:13:18 +0100

    begin value iteration

---
+++ reinforcement/valueIterationAgents.py
@@ -0,0 +1,119 @@
+# valueIterationAgents.py
+# -----------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+import mdp, util
+
+from learningAgents import ValueEstimationAgent
+
+class ValueIterationAgent(ValueEstimationAgent):
+    """
+        * Please read learningAgents.py before reading this.*
+
+        A ValueIterationAgent takes a Markov decision process
+        (see mdp.py) on initialization and runs value iteration
+        for a given number of iterations using the supplied
+        discount factor.
+    """
+    def __init__(self, mdp, discount = 0.9, iterations = 100):
+        """
+          Your value iteration agent should take an mdp on
+          construction, run the indicated number of iterations
+          and then act according to the resulting policy.
+
+          Some useful mdp methods you will use:
+              mdp.getStates()
+              mdp.getPossibleActions(state)
+              mdp.getTransitionStatesAndProbs(state, action)[((0, 1), 0.0), ((0, 2), 1.0)] lijst met node waarnaartoe + kans er te komen.
+              mdp.getReward(state, action, nextState)
+              mdp.isTerminal(state)
+        """
+        self.mdp = mdp
+        self.discount = discount
+        self.iterations = iterations
+        self.values = util.Counter() # A Counter is a dict with default 0
+
+        for state in mdp.getStates():
+            self.values[state] = 0
+
+
+        for i in range(5):
+            tempValues = self.values.copy()
+            print("in de looooooop")
+            if i == 0:
+                for state in mdp.getStates():
+                    self.values[state] = 0
+            if i == 1:
+                for state in mdp.getStates():
+                    for action in mdp.getPossibleActions(state):
+                        if action == 'exit':
+                            self.values[state] = mdp.getReward(state, action, 'TERMINAL_STATE')
+            else:
+                for state in mdp.getStates():
+                    tempMaxAction = util.Counter()
+                    #print("state: ", state)
+                    if not mdp.isTerminal(state):
+                        for action in mdp.getPossibleActions(state):
+                            if action != 'exit':
+                                for nextState, prob in mdp.getTransitionStatesAndProbs(state, action):
+                                    tempMaxAction[action] += prob * (mdp.getReward(state, action, nextState) + self.discount * self.values[nextState])
+                                #print( "argsmaxxed ervan: ", tempMaxAction[tempMaxAction.argMax()])
+                                tempValues[state] = tempMaxAction[tempMaxAction.argMax()]
+                            else:
+                                tempValues[state] = mdp.getReward(state, action, 'TERMINAL_STATE')
+                self.values = tempValues.copy()
+            print(self.values)
+
+    def getValue(self, state):
+        """
+          Return the value of the state (computed in __init__).
+        """
+        return self.values[state]
+
+
+    def computeQValueFromValues(self, state, action):
+        """
+          Compute the Q-value of action in state from the
+          value function stored in self.values.
+        """
+        "*** YOUR CODE HERE ***"
+        Qvalue = 0
+        if action != 'exit':
+            for nextState, prob in self.mdp.getTransitionStatesAndProbs(state, action):
+                Qvalue += prob * (self.mdp.getReward(state, action, nextState) + self.discount * self.values[nextState])
+            return Qvalue
+        elif not self.mdp.isTerminal(state):
+            return self.mdp.getReward(state, action, 'TERMINAL_STATE')
+        return 0
+
+    def computeActionFromValues(self, state):
+        """
+          The policy is the best action in the given state
+          according to the values currently stored in self.values.
+
+          You may break ties any way you see fit.  Note that if
+          there are no legal actions, which is the case at the
+          terminal state, you should return None.
+        """
+        "*** YOUR CODE HERE ***"
+        return self.values.argMax()
+
+    def getPolicy(self, state):
+        return self.computeActionFromValues(state)
+
+    def getAction(self, state):
+        "Returns the policy at the state (no exploration)."
+        return self.computeActionFromValues(state)
+
+    def getQValue(self, state, action):
+        return self.computeQValueFromValues(state, action)
