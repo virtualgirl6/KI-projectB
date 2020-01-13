005543c2869bdcf5d6d7b58586df8485701a1a69
virtualgirl6 <vicky.bosch@hotmail.com>
2020-01-13 16:39:45 +0100

    meer train defs

--- classification/mira.py
+++ classification/mira.py
@@ -50,7 +50,105 @@ class MiraClassifier:
 
         return self.trainAndTune(trainingData, trainingLabels, validationData, validationLabels, Cgrid)
 
+
+
+    def trainAndTune2(self, trainingData, trainingLabels, validationData, validationLabels, Cgrid):
+        bestWeights = {}
+        bestAccuracy = None
+        for c in Cgrid:
+            weights = self.weights.copy()
+            for n in range(self.max_iterations):
+                for i, datum in enumerate(trainingData):
+                    # Try to guess the label
+                    bestScore = None
+                    bestY = None
+                    for y in self.legalLabels:
+                        score = datum * weights[y]
+                        if score > bestScore or bestScore is None:
+                            bestScore = score
+                            bestY = y
+
+                    actualY = trainingLabels[i]
+                    if bestY != actualY:
+                        # Wrong guess, update weights
+                        f = datum.copy()
+                        tau = min(c, ((weights[bestY] - weights[actualY]) * f + 1.0) / (2.0 * (f * f)))
+                        f.divideAll(1.0 / tau)
+
+                        weights[actualY] = weights[actualY] + f
+                        weights[bestY] = weights[bestY] - f
+
+            # Check the accuracy associated with this c
+            correct = 0
+            guesses = self.classify(validationData)
+            for i, guess in enumerate(guesses):
+                correct += (validationLabels[i] == guess and 1.0 or 0.0)
+            accuracy = correct / len(guesses)
+
+            if accuracy > bestAccuracy or bestAccuracy is None:
+                bestAccuracy = accuracy
+                bestWeights = weights
+
+        self.weights = bestWeights
+
     def trainAndTune(self, trainingData, trainingLabels, validationData, validationLabels, Cgrid):
+        """
+        This method sets self.weights using MIRA.  Train the classifier for each value of C in Cgrid,
+        then store the weights that give the best accuracy on the validationData.
+
+        Use the provided self.weights[label] data structure so that
+        the classify method works correctly. Also, recall that a
+        datum is a counter from features to values for those features
+        representing a vector of values.
+        """
+        "*** YOUR CODE HERE ***"
+
+        trainWeights = self.weights
+        bestWeights = {}
+        bestAccuracy = None
+        c = Cgrid[2]
+        for i in range(self.max_iterations):
+            for j in range(len(trainingData)):
+
+                datum = trainingData[j]
+                vector = util.Counter()
+
+                for label in self.legalLabels:
+                    vector[label] = trainWeights[label] * datum #score
+
+                wy = trainWeights[vector.argMax()]
+                wyp = trainWeights[trainingLabels[j]]
+
+                tau = min(c,((wyp - wy) * datum + 1.0) / ( 2 * (datum * datum)))
+
+                tempDatum = datum.copy()
+
+                for d in tempDatum:
+                    tempDatum[d] *= tau
+
+                if wy != wyp:
+                    wyp += tempDatum
+                    wy -= tempDatum
+
+        correct = 0
+        guesses = self.classify(validationData)
+        for i, guess in enumerate(guesses):
+            correct += (validationLabels[i] == guess and 1.0 or 0.0)
+        accuracy = correct / len(guesses)
+
+        if accuracy > bestAccuracy or bestAccuracy is None:
+            bestAccuracy = accuracy
+            bestWeights = trainWeights
+
+        self.weights = bestWeights
+
+
+
+
+
+
+
+    def trainAndTune1(self, trainingData, trainingLabels, validationData, validationLabels, Cgrid):
         """
         This method sets self.weights using MIRA.  Train the classifier for each value of C in Cgrid,
         then store the weights that give the best accuracy on the validationData.
@@ -67,25 +165,31 @@ class MiraClassifier:
 
         for iteration in range (self.max_iterations):
             print "Starting iteration ", iteration, "..."
-            for i in range(len(trainingData)):
-                datum = trainingData[i]
-                vectors = util.Counter()
+            for c in Cgrid:
+                for i in range(len(trainingData)):
+                    datum = trainingData[i]
+                    #print(datum)
+                    vectors = util.Counter()
 
+                    for label in self.legalLabels:
+                        vectors[label] = self.weights[label] * datum
+                        #print(self.legalLabels)
 
+                        wy = self.weights[trainingLabels[i]] #= datum
+                        wyprime = self.weights[vectors.argMax()]
 
-                for label in self.legalLabels:
-                    vectors[label] = self.weights[label] * datum
-                    wy = self.weights[trainingLabels[i]]
-                    wyprime = self.weights[vectors.argMax()]
-                print(type(datum))
-                print(datum)
-                for c in Cgrid:
-                    tau = ((wyprime - wy) * datum + 1.0)/ ( 2.0 * datum * datum)
-                    while (tau < c):
-                        if vectors.argMax() != trainingLabels[i]:
-                            wy = wy + (tau * datum)
-                            wyprime = wyprime - (tau * datum)
+                        tau = ((wyprime - wy) * datum + 1) / ( 2 * (datum * datum))
 
+                        if (tau < c):
+                            if wy != wyprime: #?
+                                tau = 1
+                                datum.divideAll(1.0 / tau)
+                                wy = wy +  datum
+                                wyprime = wyprime -  datum
+                                print(wy)
+                                print(wyprime)
+                # max C ophalen
+                #datum is counter, moeten value gebruiken? oid
     def classify(self, data ):
         """
         Classifies each datum as the label that most closely matches the prototype vector
